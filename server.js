e918205fb2d7f87d306178057f979cc75f2ca532
ChiragOnGitHub <chirag.cs102@gmail.com>
2025-09-17 16:58:12 +0530

    Question uploaded

--- server.js
+++ server.js
@@ -29,6 +29,7 @@ const io = new Server(server, {
 connectDB();
 
 // Middleware
+app.use("/public", express.static(path.join(__dirname, "public")));
 app.use(cors({
   origin: process.env.CLIENT_URL,
   credentials: true
